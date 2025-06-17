87b5c3188316781c336bd071f5e436cb30240d21
Avstn1 <97896017+Avstn1@users.noreply.github.com>
2025-06-17 02:24:12 -0400

    Make proper changes to tab title

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,7 +1,6 @@
 import Navbar from './components/Navbar/Navbar';
 import ProjectCard from './components/Card/Card';
 import './globals.css';
-import Head from 'next/head';
 
 export default function Home() {
   const projects = [
@@ -37,9 +36,6 @@ export default function Home() {
 
   return (
     <div className="bg-midnight text-sand scroll-smooth">
-      <Head>
-        <title>Austin Bartolome</title>
-      </Head>
       <Navbar />
 
       {/* Hero Section */}
